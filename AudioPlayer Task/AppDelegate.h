9fc6f934b2a0f84c7741740a03a76a38bda58cbe
srikanth <eminosoft.srikanth@gmail.com>
2015-01-22 16:07:43 +0530

    Initial Commit

---
+++ AudioPlayer Task/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioPlayer Task
+//
+//  Created by Eminosoft on 22/01/15.
+//  Copyright (c) 2015 eminosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioPlayer Task/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AudioPlayer Task
+//
+//  Created by Eminosoft on 22/01/15.
+//  Copyright (c) 2015 eminosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
